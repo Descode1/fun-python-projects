05126485a27f9e1b2a0603de0b5095dd14732cd5
Davy <103334758+Descode1@users.noreply.github.com>
2025-10-29 19:54:08 +0200

    1pass refactor

--- 1pass/1pass.py
+++ 1pass/1pass.py
@@ -1,43 +1,22 @@
 import random
-nums = ["0","1","2","3","4","5","6","7","8","9"]
-lowerLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-upperLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-symbols = ["~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","}","{",":",";",">","<",".",",","/","?"]
 
-def get_password(strength):
+NUMS = "0123456789"
+LOWER = "abcdefghijklmnopqrstuvwxyz"
+UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+SYMBOLS = "~!@#$%^&*()_-+=}{:;><.,/?"
+
+CHAR_SETS = [NUMS, LOWER, UPPER, SYMBOLS]
+
+def get_password():
     password = ""
-    i = 0
-    while i < strength:
-        rand = random.randint(0,3)
-        if rand == 0:
-            char = nums[random.randint(0,len(nums)-1)]
-        elif rand == 1:
-            char = lowerLetters[random.randint(0,len(lowerLetters)-1)]
-        elif rand == 2:
-            char = upperLetters[random.randint(0,len(upperLetters)-1)]
-        else:
-            char = symbols[random.randint(0,len(symbols)-1)]
-        password += char
-        i += 1
+    for _ in range(18):
+        char_set = random.choice(CHAR_SETS)
+        password += random.choice(char_set)
     return password
 
-print("============1Pass============")
-print("Enter your required strength")
-print("1. Low")
-print("2. Mid")
-print("3. High")
-answ = input("> ")
-if int(answ) == 1:
-    passcode = get_password(12)
-    print(passcode)
-elif int(answ) == 2:
-    passcode = get_password(18)
-    print(passcode)
-elif int(answ) == 3:
-    passcode = get_password(24)
-    print(passcode)
-else:
-    print("Invalid choice!")
+print(get_password())
+
+
 
 
 
